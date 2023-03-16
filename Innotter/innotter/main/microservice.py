9dd92887da84458d9dd16ed3888018a80aa95618
KahaRinS <slavakah1@gmail.com>
2023-03-17 00:29:05 +0300

    refact: add doesNotExist exception

--- Innotter/innotter/main/microservice.py
+++ Innotter/innotter/main/microservice.py
@@ -1,10 +1,15 @@
+from django.core.exceptions import ObjectDoesNotExist
+import logging
 from main.models import Like, Page, Post
 from main.producer import publish
 
 
 def update(owner):
-    page_id = Page.objects.get(owner = owner).uuid
-    likes = Like.objects.filter(post__page__owner=owner).count()
-    posts = Post.objects.filter(page__owner = owner).count()
-    subscribers = Page.objects.get(owner = owner).followers.all().count()
+    try:
+        page_id = Page.objects.get(owner = owner).uuid
+        likes = Like.objects.filter(post__page__owner=owner).count()
+        posts = Post.objects.filter(page__owner = owner).count()
+        subscribers = Page.objects.get(owner = owner).followers.all().count()
+    except ObjectDoesNotExist:
+        logging.error('Page does not exist')
     publish('quote_created', {'page_id': page_id, 'subscribers': subscribers, 'posts': posts, 'likes': likes}, 'like')
