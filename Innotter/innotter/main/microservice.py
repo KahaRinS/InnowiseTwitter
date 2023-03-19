3c138e939d23180c1d2f0c81fd63732b551e2d41
KahaRinS <slavakah1@gmail.com>
2023-03-19 20:42:16 +0300

    refact: cahnge update method, delete some excess defs

--- Innotter/innotter/main/microservice.py
+++ Innotter/innotter/main/microservice.py
@@ -6,10 +6,12 @@ from main.producer import publish
 
 def update(owner):
     try:
-        page_id = Page.objects.get(owner = owner).uuid
-        likes = Like.objects.filter(post__page__owner=owner).count()
-        posts = Post.objects.filter(page__owner = owner).count()
-        subscribers = Page.objects.get(owner = owner).followers.all().count()
+        page = Page.objects.get(owner=owner)
+        posts_count = page.posts.count()
+        likes_count = Like.objects.filter(post__page__owner=owner).count()
+        subscribers_count = page.followers.count()
+        publish('quote_created',
+                {'page_id': page.uuid, 'subscribers': subscribers_count, 'posts': posts_count, 'likes': likes_count},
+                'like')
     except ObjectDoesNotExist:
         logging.error('Page does not exist')
-    publish('quote_created', {'page_id': page_id, 'subscribers': subscribers, 'posts': posts, 'likes': likes}, 'like')
