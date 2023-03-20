fb51b1b734dfe34c5b9e95c495469b30d41e9f1c
KahaRinS <slavakah1@gmail.com>
2023-03-20 16:00:53 +0300

    refact: delete row to fix conflict

--- Innotter/innotter/main/views.py
+++ Innotter/innotter/main/views.py
@@ -19,7 +19,6 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from innotter.tasks import my_task
 
 
 class NewsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
